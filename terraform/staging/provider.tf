388f98697942aa8b62bb0e42e3d7a4e49433b5a3
Gauna Facundo <fgauna12@gmail.com>
2021-06-28 17:02:04 -0400

    Added backend

--- terraform/staging/provider.tf
+++ terraform/staging/provider.tf
@@ -4,6 +4,12 @@ terraform {
       version = "~>2.65"
     }
   }
+
+  backend "azurerm" {
+    storage_account_name = "stterraformhellocattle01"
+    container_name       = "staging"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
